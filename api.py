befe3678a7813941957392472093d690b1299aba
zlinSG <zlin@monarchtractor.com>
2023-02-21 17:33:37 +0800

    add onnx prediction for flask api

--- api.py
+++ api.py
@@ -5,11 +5,18 @@
 import os
 import torch
 import json 
+import time 
+
+
 import albumentations as A
 import pretrainedmodels
 import argparse
+import pprint
 import numpy as np
 import torch.nn as nn
+import onnx 
+import onnxruntime as ort
+
 
 from flask import Flask
 from flask import request
@@ -35,11 +42,11 @@ def validate_image(file_path):
         return False
 
 
-def predict(image_path, model, device):
+def predict(image_path, height, width, model, device, use_onnx):
     test_datasets = Custom_dataset(
         image_list = [image_path], \
         label_list = [0], \
-        transform=image_transforms['test']
+        transform=image_transforms(height = height, width = width, phase = 'test')
     )
     # print('image_path', image_path)
     # print('len of test_dataset: ', len(test_datasets))
@@ -50,10 +57,24 @@ def predict(image_path, model, device):
         shuffle=False,
         num_workers=0
     )
+
+    # check the time difference 
+    # torch.cuda.synchronize()
+    # start = time.time()
+
     for img, _ in test_loader:
         img = img.to(device)
-        _, out = model(img)  # out.shape = batchsize, num_classes
+   
+        if use_onnx:
+            input_name = model.get_inputs()[0].name
+            # label_name = model.get_outputs()[0].name
+            out = model.run(None, {input_name: img.cpu().numpy()})[-1]  # onnx model is for running on CPU -- its output is numpy 
+            out = torch.tensor(out)
+        else:
+            _, out = model(img)  # out.shape = batchsize, num_classes
         predict_label = torch.argmax(out, dim = -1)  # batch, 1
+
+        # print('time: ', (time.time() - start))
         # print('predict result', predict_label.item(), label_list[predict_label.item()])
         return predict_label.item()
 
@@ -66,7 +87,6 @@ def upload_predict():
         
         image_file = request.files["image"]
 
-        
         if image_file:
             image_location = os.path.join(
                 upload_folder,
@@ -79,10 +99,8 @@ def upload_predict():
                 raise ValueError('Invalid image file')
             else:
                 logger.info('passed the image check')
-            
-            
 
-            pred_index = predict(image_location, model, device)  # label index
+            pred_index = predict(image_location, h, w, model, device, use_onnx)  # label index
             pred_label = label_list[pred_index]
             logger.info(f'prediction result: {pred_index}, {pred_label}')
 
@@ -98,16 +116,19 @@ if __name__ == "__main__":
     parser.add_argument('--config', default = 'basic.yml', help = 'yaml file for training')
 
     args = parser.parse_args()
-    logger.info('get args: %s', args)
+    # logger.info('get args: %s', args)
+
 
     with open(args.config, 'r') as ff:
         cfg = yaml.safe_load(ff)
+    logger.info(pprint.pformat(args))
+    logger.info(pprint.pformat(cfg))
 
     label_list = list(json.load(open(cfg['saved_label_path'])).keys())
     logger.info(f'label_list: {" ".join(i for i in label_list)}')
 
     upload_folder = cfg['API']['upload_folder']
-    device = cfg['API']['device']
+    device = 'cuda' if cfg['API']['device'] == 'cuda' and torch.cuda.is_available() else 'cpu'
     logger.info(f'upload_folder: {upload_folder}, device: {device}')
 
 
@@ -116,5 +137,30 @@ if __name__ == "__main__":
     model.load_state_dict(model_state)
     model.to(device)
     logger.info('Loaded model!')
+
+    use_onnx = cfg['onnx']['use_onnx']
+    w = cfg['API']['width']
+    h = cfg['API']['height']
+    if use_onnx:
+        logger.info('use onnx model')
+
+        onnx_model_path = cfg['onnx']['saved_onnx_path']
+        if not os.path.exists(onnx_model_path):
+            logger.info('not find onnx model. export it now')
+
+            torch.onnx.export(model, \
+                            torch.randn(1, 3, w, h).to(device), \
+                            onnx_model_path, \
+                            export_params=True, \
+                            opset_version=13, \
+                            input_names = ['input'], \
+                            output_names = ['features', 'output'])
+        
+        onnx_model = onnx.load(onnx_model_path)
+        onnx.checker.check_model(onnx_model)
+
+        providers= ['CUDAExecutionProvider'] if device == 'cuda' else ['CPUExecutionProvider'] 
+        model  = ort.InferenceSession(onnx_model_path, providers = providers)
+        
 
     app.run(host="0.0.0.0", port=12000, debug=True)
